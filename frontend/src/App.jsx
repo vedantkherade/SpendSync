2d897c95cc503aef95a3fd0cb68695fbf9fffa15
vedantkherade <vedantkherade76@gmail.com>
2026-04-04 01:50:51 +0530

    dashboard layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import { useState } from "react";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const navigate = useNavigate();
+
+  const clearAuth = () => {
+    try {
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      sessionStorage.removeItem("user");
+      sessionStorage.removeItem("token");
+    } 
+    
+    catch (error) {
+      console.error("clearAuth error", err);
+    }
+    setUser(null);
+    setToken(null);
+  }
+
+  const handleLogout = () => {
+    clearAuth();
+    navigate("/login");
+  }
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+    <Routes>
+       <Route element={<Layout/>}>
+          <Route path="/" element={<Dashboard/>}/>
+       </Route>
+    </Routes>
+    </>
   );
 };
 
